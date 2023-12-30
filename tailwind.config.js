6eac63ed0cba0e0a441f5c9e406dcc3f92284c4e
Allllllllllll <137359388+Alizaaaja4@users.noreply.github.com>
2023-12-30 18:59:37 +0700

    firts idea

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        permanent: "#14b8a6",
+        standar: "#020617",
+        primary: "#1e293b",
+        secondary: "#f8fafc",
+      }
+    },
+  },
+  plugins: [],
+}
+
